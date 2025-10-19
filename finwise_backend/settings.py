903432e447d8dc2fcd346e18b16afd8411490693
Demaurr <69788671+Demaurr@users.noreply.github.com>
2025-10-20 01:01:10 +0500

    Moved from sqlite3 to PostgreSql

--- finwise_backend/settings.py
+++ finwise_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 from datetime import timedelta
+import dj_database_url
 
 
 
@@ -91,10 +92,11 @@ WSGI_APPLICATION = "finwise_backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
